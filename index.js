3e11d5f89827038ed5d6ae18849e448d26b500e1
sonali_sohoni <ssohoni@camsbycbs.com>
2021-11-12 23:40:14 -0500

    code refactor

--- index.js
+++ index.js
@@ -26,7 +26,14 @@ inquirer
 	.then((res) => {
 		const opt_select = res.options;
 		if (opt_select === "View All Departments") {
-			printResults(opt_select, "select * from department");
+			//	getQueryResults("select * from department");
+
+			getQueryResults(opt_select, "select * from department").then(
+				(results) => {
+					console.table("my returned results");
+					console.table(results);
+				}
+			);
 		} else if (opt_select === "View All Roles") {
 			const sq = `SELECT role.id,role.title as job_title,role.salary,department.dept_name FROM company_db.role left join department on role.department_id = department.id`;
 			printResults(opt_select, sq);
@@ -36,21 +43,140 @@ employee e  left join role r on e.role_id = r.id
 left join department d on r.department_id = d.id
 left join employee m on e.manager_id = m.id`;
 			printResults(opt_select, sq);
+		} else if (opt_select === "Add A Department") {
+			addNewDepartment();
+		} else if (opt_select === "Add A Role") {
+			addNewRole();
 		}
 	});
 
-const printResults = function (opt_select, sql) {
-	db.promise()
+// const getQueryResults = function (opt_select, sql) {
+// 	let results;
+
+// 	db.promise()
+// 		.query(sql)
+// 		.then(([rows, fields]) => {
+// 			console.log(rows);
+// 			results = rows;
+// 		})
+// 		.catch(console.log)
+// 		.then(() => db.end());
+// 	// .catch(console.log)
+// 	// .then(() => db.end());
+
+// 	return results;
+// };
+
+const getQueryResults = (opt_select, sql) => {
+	return db
+		.promise()
 		.query(sql)
-		.then(([rows, fields]) => {
-			console.table(opt_select + "\n");
-			console.table(rows);
-		})
-		.catch(console.log)
-		.then(() => db.end());
+		.then(([results, fields]) => {
+			return results;
+		});
 };
 
-// db.query("SELECT * FROM departments", function (err, results) {
-// 	console.log(results);
-// 	console.log(err);
-// });
+const printResults = (opt_select, sql) => {
+	return db
+		.promise()
+		.query(sql)
+		.then(([results, fields]) => {
+			return results;
+		});
+};
+
+const addNewDepartment = function () {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "department",
+				message: "Please enter the name of the department you want to add -",
+				validate: (arg) => {
+					if (!arg) {
+						return false;
+					} else return true;
+				},
+			},
+		])
+		.then((data) => {
+			const sql = "insert into department (dept_name) values (?)";
+			const params = [data.department];
+			db.promise()
+				.query(sql, params)
+				.then(([rows, fields]) => {
+					if (!rows.affectedRows) {
+						console.log("Can not add new department");
+						return;
+					} else {
+						console.log("successfully inserted new department.");
+						printResults("View All Departments", "select * from department");
+					}
+				})
+				.catch(console.log)
+				.then(() => db.end());
+		});
+};
+
+const addNewRole = function () {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "role",
+				message: "Please enter the name of the role you want to add -",
+				validate: (arg) => {
+					if (!arg) {
+						return false;
+					} else return true;
+				},
+			},
+			{
+				type: "input",
+				name: "salary",
+				message: "Please enter the name of the salary for this role -",
+				validate: (arg) => {
+					if (!arg) {
+						return false;
+					} else return true;
+				},
+			},
+			{
+				type: "list",
+				name: "dept",
+				message:
+					"Please enter the name of the departmet this role falls under -",
+				validate: (arg) => {
+					if (!arg) {
+						return false;
+					} else return true;
+				},
+				choices: [
+					"1-HR",
+					"2-Engineering",
+					"3-Purchase",
+					"4-Sales",
+					"5-Plant Operations",
+					"6-Admin Operations",
+				],
+			},
+		]) // insert into role(title,salary,department_id) values
+
+		.then((data) => {
+			const sql = "insert into department (dept_name) values (?)";
+			const params = [data.department];
+			db.promise()
+				.query(sql, params)
+				.then(([rows, fields]) => {
+					if (!rows.affectedRows) {
+						console.log("Can not add new department");
+						return;
+					} else {
+						console.log("successfully inserted new department.");
+						printResults("View All Departments", "select * from department");
+					}
+				})
+				.catch(console.log)
+				.then(() => db.end());
+		});
+};
