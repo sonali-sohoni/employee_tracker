7671481df17d7c2800cd5766cc5513d45f282d62
sonali_sohoni <ssohoni@camsbycbs.com>
2021-11-14 01:47:39 -0500

    refactoring complete

--- index.js
+++ index.js
@@ -6,9 +6,59 @@ const { error } = require("console");
 const { fsyncSync } = require("fs");
 
 //Display menu with inquirer
-console.log("Welcome to Employee Tracker System!!");
-inquirer
-	.prompt([
+function startApp() {
+	console.log(`
+  =================================================
+  
+        Welcome to Employee Tracker System!!
+  
+  =================================================
+  
+  `);
+	getUserPrompts()
+		.then((res) => {
+			const opt_select = res.options;
+			console.log(1, opt_select, opt_select === "View All Roles");
+			processUserPrompts(opt_select);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
+
+const processUserPrompts = (opt_select) => {
+	if (opt_select === "View All Departments") {
+		printResults(opt_select, "select * from department");
+	} else if (opt_select === "View All Roles") {
+		console.log("2");
+		const sq = `SELECT role.id,role.title as job_title,role.salary,department.dept_name FROM company_db.role left join department on role.department_id = department.id`;
+		printResults(opt_select, sq);
+		console.log("v1");
+	} else if (opt_select === "View All Employees") {
+		const sq = `select e.id,e.first_name,e.last_name ,r.title,d.dept_name,r.salary, concat(m.first_name, " ",m.last_name) as manager from
+employee e  left join role r on e.role_id = r.id
+left join department d on r.department_id = d.id
+left join employee m on e.manager_id = m.id`;
+		printResults(opt_select, sq);
+		// getQueryResults(opt_select, sq).then((results) => {
+		// 	console.table("my returned results");
+		// 	console.table(results);
+		// });
+		//	printResults(opt_select, sq);
+	} else if (opt_select === "Add A Department") {
+		addNewDepartment();
+	} else if (opt_select === "Add A Role") {
+		addNewRole();
+	} else if (opt_select === "Add An Employee") {
+		addNewEmployee();
+	} else if (opt_select === "Update Employee's Role") {
+		updateEmployeeRole();
+	} else if (opt_select === "Exit") {
+		return;
+	}
+};
+const getUserPrompts = () => {
+	return inquirer.prompt([
 		{
 			type: "list",
 			name: "options",
@@ -21,41 +71,12 @@ inquirer
 				"Add A Role",
 				"Add An Employee",
 				"Update Employee's Role",
+				"View Employees By Manager",
+				"Exit",
 			],
 		},
-	])
-	.then((res) => {
-		const opt_select = res.options;
-		if (opt_select === "View All Departments") {
-			//	getQueryResults("select * from department");
-
-			//
-			printResults(opt_select, "select * from department");
-		} else if (opt_select === "View All Roles") {
-			const sq = `SELECT role.id,role.title as job_title,role.salary,department.dept_name FROM company_db.role left join department on role.department_id = department.id`;
-			printResults(opt_select, sq);
-			//	printResults(opt_select, sq);
-		} else if (opt_select === "View All Employees") {
-			const sq = `select e.id,e.first_name,e.last_name ,r.title,d.dept_name,r.salary, concat(m.first_name, " ",m.last_name) as manager from
-employee e  left join role r on e.role_id = r.id
-left join department d on r.department_id = d.id
-left join employee m on e.manager_id = m.id`;
-			printResults(opt_select, sq);
-			// getQueryResults(opt_select, sq).then((results) => {
-			// 	console.table("my returned results");
-			// 	console.table(results);
-			// });
-			//	printResults(opt_select, sq);
-		} else if (opt_select === "Add A Department") {
-			addNewDepartment();
-		} else if (opt_select === "Add A Role") {
-			addNewRole();
-		} else if (opt_select === "Add An Employee") {
-			addNewEmployee();
-		} else if (opt_select === "Update Employee's Role") {
-			updateEmployeeRole();
-		}
-	});
+	]);
+};
 
 const getQueryResults = (sql) => {
 	return db
@@ -63,26 +84,41 @@ const getQueryResults = (sql) => {
 		.query(sql)
 		.then(([results, fields]) => {
 			return results;
-		})
-		.catch((e) => {
-			console.log(e);
 		});
 };
 
 const printResults = (opt_select, sql) => {
 	getQueryResults(sql)
 		.then((results) => {
-			console.table(
-				"-------------------------------------------------------------------------\n"
-			);
-			console.table(opt_select);
-			console.table(
-				"\n-------------------------------------------------------------------------\n"
-			);
+			console.table(`\n-------------------------------------------------------------------------
+		
+						${opt_select}
+		
+-------------------------------------------------------------------------`);
 			console.table(results);
+			return getContinuePrompt();
 		})
+
 		.catch((err) => {
 			console.log(err);
+		});
+};
+
+const getContinuePrompt = () => {
+	return inquirer
+		.prompt([
+			{
+				type: "confirm",
+				name: "continue",
+				message: "Want to continue?",
+			},
+		])
+		.then((result) => {
+			if (result.continue) {
+				startApp();
+			} else {
+				db.end();
+			}
 		});
 };
 
@@ -121,12 +157,18 @@ const addNewDepartment = function () {
 
 const addNewRole = function () {
 	let dept_arr = [];
+	let dept_arr2 = [];
+	let dept_names = [];
 	getQueryResults("select * from department").then((results) => {
 		//	console.log(results);
-
 		results.forEach((obj) => {
-			dept_arr.push(obj.id + "_" + obj.dept_name);
+			dept_arr2.push(obj);
+			dept_names.push(obj.dept_name);
 		});
+		console.log(dept_arr2);
+		// results.forEach((obj) => {
+		// 	dept_arr.push(obj.id + "_" + obj.dept_name);
+		// });
 		//	console.log(dept_arr);
 	});
 	inquirer
@@ -161,13 +203,16 @@ const addNewRole = function () {
 						return false;
 					} else return true;
 				},
-				choices: dept_arr,
+				choices: dept_names,
 			},
 		]) // insert into role(title,salary,department_id) values
 
 		.then((data) => {
 			//(" Engineer",80000.00,2),
-			const deptid = data.dept.split("_")[0];
+			//	const deptid = data.dept.split("_")[0];
+			const deptindex = dept_names.findIndex((val) => val === data.dept);
+			const deptid = dept_arr2[deptindex].id;
+			console.log("Selected deptid =" + deptid);
 			const sql = "insert into role(title,salary,department_id) values (?,?,?)";
 			const params = [data.role, data.salary, deptid];
 			db.promise()
@@ -177,36 +222,48 @@ const addNewRole = function () {
 						console.log("Can not add new department");
 						return;
 					} else {
-						console.log("successfully inserted new department.");
-						printResults("View All Departments", "select * from department");
+						console.log("successfully added new role.");
+						printResults("View All Roles", "select * from role");
 					}
 				})
-				.catch(console.log)
-				.then(() => db.end());
+				.catch((err) => {
+					console.log(err);
+				});
 		});
 };
 
 const addNewEmployee = function () {
-	let role_arr = [];
-	let emp_arr = [];
-	console.log(11);
+	let role_arr2 = [];
+	let role_names = [];
+
+	let emp_arr2 = [];
+	let emp_names = [];
+	//console.log(11);
 	getQueryResults("select * from role").then((results) => {
 		//console.log(results);
-
 		results.forEach((obj) => {
-			role_arr.push(obj.id + "_" + obj.title);
+			role_arr2.push(obj);
+			role_names.push(obj.title);
 		});
+		//console.log(role_arr2);
+		// results.forEach((obj) => {
+		// 	role_arr.push(obj.id + "_" + obj.title);
+		// });
 		//	console.log(dept_arr);
 	});
 	getQueryResults("select * from employee").then((results) => {
 		//console.log(results);
 
+		// results.forEach((obj) => {
+		// 	emp_arr.push(obj.id + "_" + obj.first_name + " " + obj.last_name);
+		// });
 		results.forEach((obj) => {
-			emp_arr.push(obj.id + "_" + obj.first_name + " " + obj.last_name);
+			emp_arr2.push(obj);
+			emp_names.push(obj.first_name + " " + obj.last_name);
 		});
-		console.log(emp_arr);
+		//	console.log(emp_arr2);
 	});
-	emp_arr.push("none");
+	emp_names.push("none");
 	inquirer
 		.prompt([
 			{
@@ -238,23 +295,30 @@ const addNewEmployee = function () {
 						return false;
 					} else return true;
 				},
-				choices: role_arr,
+				choices: role_names,
 			},
 			{
 				type: "list",
 				name: "manager",
 				message:
 					"Please select the employee's manager - select none if employee does not have manager assigned.",
-				choices: emp_arr,
+				choices: emp_names,
 			},
 		])
 
 		.then((data) => {
 			console.log(data);
 			// 		//(" Engineer",80000.00,2),
-			const roleid = data.role.split("_")[0];
+			//const roleid = data.role.split("_")[0];
+			const roleindex = role_names.findIndex((val) => val === data.role);
+			const roleid = role_arr2[roleindex].id;
+			console.log("Selected roleid =" + roleid);
 			let managerid = null;
-			if (data.manager !== "none") managerid = data.manager.split("_")[0];
+			if (data.manager !== "none") {
+				const empindex = emp_names.findIndex((val) => val === data.manager);
+				managerid = emp_arr2[empindex].id;
+				console.log("Selected empid =" + managerid);
+			} //managerid = data.manager.split("_")[0];
 			let sql =
 				"insert into employee (first_name,last_name,role_id,manager_id)values (?,?,?,?)";
 			let params = [data.fname, data.lname, roleid, managerid];
@@ -275,7 +339,7 @@ const addNewEmployee = function () {
 						console.log("Can not add new employee");
 						return;
 					} else {
-						console.log("successfully inserted new employee.");
+						console.log("successfully added new employee.");
 						printResults("View All Departments", "select * from employee");
 					}
 				})
@@ -286,12 +350,17 @@ const addNewEmployee = function () {
 const updateEmployeeRole = function () {
 	let role_arr = [];
 	let emp_arr = [];
+	let role_arr2 = [];
+	let role_names = [];
+
+	let emp_arr2 = [];
+	let emp_names = [];
 	const questions = [
 		{
 			type: "list",
 			name: "emp",
 			message: "Please select the employee from the list -",
-			choices: emp_arr,
+			choices: emp_names,
 		},
 		{
 			type: "list",
@@ -302,7 +371,7 @@ const updateEmployeeRole = function () {
 					return false;
 				} else return true;
 			},
-			choices: role_arr,
+			choices: role_names,
 		},
 	];
 
@@ -310,28 +379,41 @@ const updateEmployeeRole = function () {
 		.then((results) => {
 			//console.log(results);
 
+			// results.forEach((obj) => {
+			// 	role_arr.push(obj.id + "_" + obj.title);
+			// });
 			results.forEach((obj) => {
-				role_arr.push(obj.id + "_" + obj.title);
+				role_arr2.push(obj);
+				role_names.push(obj.title);
 			});
 			console.log(0);
-			console.log(role_arr);
+			console.log(role_arr2);
 			return getQueryResults("select * from employee");
 		})
 		.then((results) => {
-			console.log(1);
-			console.log(results);
+			// console.log(1);
+			// console.log(results);
 
+			// results.forEach((obj) => {
+			// 	emp_arr.push(obj.id + "_" + obj.first_name + " " + obj.last_name);
+			// });
 			results.forEach((obj) => {
-				emp_arr.push(obj.id + "_" + obj.first_name + " " + obj.last_name);
+				emp_arr2.push(obj);
+				emp_names.push(obj.first_name + " " + obj.last_name);
 			});
-
+			console.log(emp_arr2);
 			return inquirer.prompt(questions);
 		})
 		.then((data) => {
 			console.log(data);
 			// 		//(" Engineer",80000.00,2),
-			const roleid = data.role.split("_")[0];
-			empid = data.emp.split("_")[0];
+			//	const roleid = data.role.split("_")[0];
+			const roleindex = role_names.findIndex((val) => val === data.role);
+			const roleid = role_arr2[roleindex].id;
+			const empindex = emp_names.findIndex((val) => val === data.emp);
+			const empid = emp_arr2[empindex].id;
+			console.log("Selected empid =" + empid);
+			//	empid = data.emp.split("_")[0];
 			let sql = "update employee set role_id = ? where id = ?";
 			let params = [roleid, empid];
 
@@ -349,7 +431,51 @@ const updateEmployeeRole = function () {
 			}
 		})
 		.catch((err) => {
-			console.log(e);
+			console.log(err);
 			db.end();
 		});
 };
+
+const viewEmployeesByManager = () => {
+	let emp_arr2 = [];
+	let emp_names = [];
+	const question = [
+		{
+			type: "list",
+			name: "emp",
+			message: "Please select the employee from the list -",
+			choices: emp_names,
+		},
+	];
+	getQueryResults("select * from employee")
+		.then((results) => {
+			results.forEach((obj) => {
+				emp_arr2.push(obj);
+				emp_names.push(obj.first_name + " " + obj.last_name);
+			});
+			console.log(emp_arr2);
+			return inquirer.prompt(question);
+		})
+		.then((data) => {
+			console.log(data);
+			const empindex = emp_names.findIndex((val) => val === data.emp);
+			const empid = emp_arr2[empindex].id;
+			console.log("Selected empid =" + empid);
+
+			let sql = `select * from employee m  left join employee e on e.id = m.manager_id where m.manager_id  IS NOT NULL and m.manager_id =?;`;
+			let params = [empid];
+
+			console.log(sql, params);
+			return db.promise().query(sql, params);
+		})
+		.then(([rows, fields]) => {
+			console.table(`\n-------------------------------------------------------------------------
+		
+						${opt_select}
+		
+-------------------------------------------------------------------------`);
+			return getContinuePrompt();
+		});
+};
+
+startApp();
